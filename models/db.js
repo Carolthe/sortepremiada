264d9c778942c9ffd1d478687db32cf063e7faae
Caroline Tenorio <carololiveiratenorio21@gmail.com>
2026-05-20 13:57:54 +0100

    Alteração backend

--- models/db.js
+++ models/db.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool(process.env.URL_MYSQL);
+const db = mysql.createPool({
+  uri: process.env.MYSQL_URL
+});
 
 module.exports = db;
 
