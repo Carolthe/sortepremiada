fa740d1f709edadd8bfc514fd518c4d45ae3817a
Caroline Tenorio <carololiveiratenorio21@gmail.com>
2026-05-20 15:01:52 +0100

    Alteração backend

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
+console.log("MYSQLHOST =", process.env.MYSQLHOST);
+console.log("MYSQLPORT =", process.env.MYSQLPORT);
+console.log("MYSQLUSER =", process.env.MYSQLUSER);
+
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
